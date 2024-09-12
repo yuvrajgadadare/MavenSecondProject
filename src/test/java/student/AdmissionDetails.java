3e6c8cea4e47041a36e12d01c6e7da0a47f38822
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-09-12 19:03:13 +0530

    Initial commit

---
+++ src/test/java/student/AdmissionDetails.java
@@ -0,0 +1,15 @@
+package student;
+
+public class AdmissionDetails {
+
+	public void FeesSubmit() {
+		System.out.println("Submitted the exam fees");
+	}
+	public void GiveExam() {
+		System.out.println("Give Exam is Called");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
